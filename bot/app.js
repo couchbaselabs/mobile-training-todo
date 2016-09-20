65d6095850c34e9ecfeba236f1e2c1217e6ea553
James Nocentini <james.nocentini@gmail.com>
2016-09-20 15:19:36 +0100

    bot to add images

---
+++ bot/app.js
@@ -0,0 +1,66 @@
+var Swagger = require('swagger-client')
+  , fs = require('fs');
+
+// Use the SwaggerJS module to dynamically load the Swagger spec
+new Swagger({
+  url: 'http://developer.couchbase.com/mobile/swagger/sync-gateway-admin/spec.json',
+  usePromise: true
+})
+  .then(function (res) {
+    client = res;
+    
+    // Start getting changes at seq: 2
+    getChanges(2);
+    
+    function getChanges(seq) {
+      // Use the Swagger client to connect to the changes feed
+      client.database.get_db_changes({db: 'todo', include_docs: true, since: seq, feed: 'longpoll'})
+        .then(function (res) {
+          var results = res.obj.results;
+          console.log(results.length + ' change(s) received');
+          processChanges(results);
+          // Get changes since the last sequence
+          getChanges(res.obj.last_seq);
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    }
+    
+    function processChanges(results) {
+      for (var i = 0; i < results.length; i++) {
+        var doc = results[i].doc;
+        var img;
+        if (!doc._deleted && doc.type == 'task' && !doc._attachments) {
+          switch (doc.task.toLowerCase()) {
+            case 'apple':
+              img = fs.readFileSync('apple.png');
+              break;
+            case 'coffee':
+              img = fs.readFileSync('coffee.png');
+              break;
+            case 'potatoes':
+              img = fs.readFileSync('potatoes.png');
+              break;
+          }
+          if (img) {
+            var base64 = img.toString('base64');
+            doc._attachments = {
+              image: {
+                content_type: 'image\/png',
+                data: base64
+              }
+            };
+            client.database.post_db_bulk_docs({db: 'todo', BulkDocsBody: {docs: [doc]}})
+              .then(function (res) {
+                getChanges(results[i].seq);
+              })
+              .catch(function (err) {
+                console.log(err);
+              });
+          }
+        }
+      }
+    }
+
+  });
