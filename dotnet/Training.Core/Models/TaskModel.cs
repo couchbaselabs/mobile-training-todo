a589564fd4a4df39721844e2deae75c1061f9ee5
Jim Borden <jim.borden@couchbase.com>
2016-10-28 07:28:09 +0900

    Add some safety around getting the image digest

--- dotnet/Training.Core/Models/TaskModel.cs
+++ dotnet/Training.Core/Models/TaskModel.cs
@@ -92,7 +92,14 @@ namespace Training.Core
         {
             _document = CoreApp.Database.GetExistingDocument(documentID);
             _name = new Lazy<string>(() => _document.GetProperty<string>("task"), LazyThreadSafetyMode.None);
-            _imageDigest = new Lazy<string>(() => _document.CurrentRevision.GetAttachment("image")?.Metadata?["digest"] as string, LazyThreadSafetyMode.None);
+            _imageDigest = new Lazy<string>(() => {
+                var metadata = _document.CurrentRevision.GetAttachment("image")?.Metadata;
+                if(metadata?.ContainsKey("digest") != true) {
+                    return null;
+                }
+
+                return _document.CurrentRevision.GetAttachment("image")?.Metadata?["digest"] as string;
+            }, LazyThreadSafetyMode.None);
         }
 
         #endregion
