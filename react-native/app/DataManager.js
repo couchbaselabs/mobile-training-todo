66e7f3e1b2bd809a8d41c6f637f98f011edd78f5
Nick Pomfret <npomfret@users.noreply.github.com>
2017-01-05 20:40:23 +1100

    cleanup react-native demo (#89)
    Changes:
    - use console.warn
    - removed call to this.setupQuery which doesn't exist
    - removed unused newPassword parameter
    - removed unused var exports =
    - made use of the unused SYNC_ENABLED field
    - use TouchableOpacity for better feedback

--- react-native/app/DataManager.js
+++ react-native/app/DataManager.js
@@ -1,12 +1,13 @@
 'use strict';
 
-import {Actions} from 'react-native-router-flux';
-import Couchbase from 'react-native-couchbase-lite';
+import {Actions} from "react-native-router-flux";
+import Couchbase from "react-native-couchbase-lite";
 
 global.LOGIN_FLOW_ENABLED = false;
 const SYNC_ENABLED = false;
 const SG_HOST = 'localhost:4984/todo';
 const USE_PREBUILT_DB = false;
+
 const VIEWS = {
   views: {
     listsByName: {
@@ -22,7 +23,7 @@ const VIEWS = {
           emit(doc.taskList.id, null);
         }
       }.toString(),
-      reduce: function(keys, values, rereduce) {
+      reduce: function (keys, values, rereduce) {
         return values.length;
       }.toString()
     },
@@ -43,33 +44,40 @@ const VIEWS = {
   }
 };
 
-var exports = module.exports = {
+module.exports = {
   init(client) {
     global.manager = client;
   },
+
   login(username, password) {
     this.startSession(username, password, null);
   },
-  startSession(username, password, newPassword) {
+
+  startSession(username, password) {
     this.installPrebuiltDb();
     global.DB_NAME = username;
     this.startDatabaseOperations()
-      .then(res => this.setupReplications(username, password))
+      .then(() => {
+        this.setupReplications(username, password)
+      })
       .then(res => Actions.lists({owner: username}));
   },
+
   setupDatabase() {
     manager.database.put_db({db: DB_NAME})
-      .then(res => this.startDatabaseOperations())
-      .catch(e => console.log('ERROR', e));
+      .then(() => this.startDatabaseOperations())
+      .catch(e => console.warn(e));
   },
+
   installPrebuiltDb() {
     if (USE_PREBUILT_DB) {
       Couchbase.installPrebuiltDatabase(DB_NAME);
     }
   },
+
   startDatabaseOperations() {
     return manager.database.get_db({db: DB_NAME})
-      .then(res => {
+      .then(() => {
         this.setupViews();
       })
       .catch(e => {
@@ -78,22 +86,24 @@ var exports = module.exports = {
         }
       });
   },
+
   setupViews() {
     manager.query.get_db_design_ddoc({db: DB_NAME, ddoc: 'main'})
       .catch(e => {
         if (e.status == 404) {
           manager.query.put_db_design_ddoc({ddoc: 'main', db: DB_NAME, body: VIEWS})
-            .catch(e => console.log('ERROR', e));
+            .catch(e => console.warn(e));
         }
-      })
-      .then(() => {
-        this.setupQuery()
       });
   },
+
   setupReplications(username, password) {
-    const SG_URL = `http://${username}:${password}@${SG_HOST}`;
-    return manager.server.post_replicate({body: {source: SG_URL, target: DB_NAME, continuous: true}})
-      .then(res => manager.server.post_replicate({body: {source: DB_NAME, target: SG_URL, continuous: true}}))
-      .catch(e => console.log('ERROR', e));
-  },
+    if(SYNC_ENABLED) {
+      const sgUrl = `http://${username}:${password}@${SG_HOST}`;
+
+      return manager.server.post_replicate({body: {source: sgUrl, target: DB_NAME, continuous: true}})
+        .then(res => manager.server.post_replicate({body: {source: DB_NAME, target: sgUrl, continuous: true}}))
+        .catch(e => console.warn(e));
+    }
+  }
 };
