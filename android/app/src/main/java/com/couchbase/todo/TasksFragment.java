fc3bdc4b4a4901c3f5368d37379ecf1d06b79351
James Nocentini <james.nocentini@gmail.com>
2017-02-07 21:58:41 +0000

    Revert "Fix #98"
    This reverts commit 90ac33cf1b6d3cf105782798eab90cd4b369d80c.

--- android/app/src/main/java/com/couchbase/todo/TasksFragment.java
+++ android/app/src/main/java/com/couchbase/todo/TasksFragment.java
@@ -125,16 +125,14 @@ public class TasksFragment extends Fragment {
                         Map<String, Object> taskList = (Map<String, Object>) document.get("taskList");
                         String listId = (String) taskList.get("id");
                         String task = (String) document.get("task");
-                        Long createdAt = (Long) document.get("createdAt");
-                        ArrayList<Object> key = new ArrayList<Object>();
+                        ArrayList<String> key = new ArrayList<String>();
                         key.add(listId);
-                        key.add(createdAt);
                         key.add(task);
                         String value = (String) document.get("_rev");
                         emitter.emit(key, value);
                     }
                 }
-            }, "1.0");
+            }, "5.0");
         }
 
         Query query = view.createQuery();
