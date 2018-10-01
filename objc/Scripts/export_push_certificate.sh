3ca374de3b03e64c51f695691c5421e1240748f8
Pasin Suriyentrakorn <pasin@couchbase.com>
2018-10-01 14:14:25 -0700

    Fix export_push_certificate.sh

---
+++ objc/Scripts/export_push_certificate.sh
@@ -0,0 +1,1 @@
+openssl pkcs12 -in Certificates/PushCertificates.p12 -out Certificates/PushCertificates.pem -nodes -clcerts
--- swift/Scripts/export_push-certificate.sh
+++
@@ -1,1 +0,0 @@
-pkcs12 -in Certificates/PushCertificates.p12 -out Certificates/PushCertificates.pem -nodes -clcerts
