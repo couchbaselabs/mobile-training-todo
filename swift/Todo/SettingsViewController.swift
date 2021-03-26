b5a39b29ae8416e033fccc769e4ef3a25b61f89d
jayahariv <10448770+jayahariv@users.noreply.github.com>
2021-03-25 18:21:27 -0700

    settings page

---
+++ swift/Todo/SettingsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SettingsViewController.swift
+//  Todo
+//
+//  Created by Jayahari Vavachan on 3/24/21.
+//  Copyright © 2021 Couchbase. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
