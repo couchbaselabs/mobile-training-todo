282837a5c229a8a0d46e30957d87f2888895661c
G. Blake Meike <blake.meike@couchbase.com>
2023-04-24 10:26:15 -0700

    fix .gitignore

---
+++ java-ws/server/Todo/src/main/java/com/couchbase/lite/todo/Logger.java
@@ -0,0 +1,40 @@
+//
+// Copyright (c) 2023 Couchbase, Inc All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package com.couchbase.lite.todo;
+
+import java.io.PrintStream;
+
+import com.couchbase.lite.ConsoleLogger;
+import com.couchbase.lite.LogDomain;
+import com.couchbase.lite.LogLevel;
+
+
+public class Logger {
+    private Logger() { }
+
+    private static ConsoleLogger logger;
+
+    public static void setLogger(ConsoleLogger logr) { logger = logr; }
+
+    public static void log(String message) { log(message, null); }
+
+    public static void log(String message, Throwable err) {
+        if (null == logger) { return; }
+        logger.log(LogLevel.INFO, LogDomain.DATABASE, message);
+        final PrintStream logStream = ConsoleLogger.getLogStream(LogLevel.INFO);
+        if (err != null) { err.printStackTrace(logStream); }
+    }
+}
